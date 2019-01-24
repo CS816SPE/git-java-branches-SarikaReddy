dda78661604d1c95a798c4facb17cdf97ce5f767
SarikaReddy <teegalasarika27@gmail.com>
2019-01-24 09:42:58 +0530

    Modified Calculator

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,13 @@ public class Calculator{
 		float a, b, res;
 		char choice, ch;
 		Scanner scan = new Scanner(System.in);
-		do{
 			System.out.print("1. ADD 2 numbers\n");
-			System.out.print("2. Exit\n\n");
+			System.out.print("2. MULTIPLY 2 numbers");
+			System.out.print("3. SUBTRACT 2 numbers");
+			System.out.print("4. DIVIDE 2 numbers");
+			System.out.print("5. Exit\n\n");
 			System.out.print("Enter your choice: ");
-			choics = scan.next().charAt(0);
+			choice = scan.next().charAt(0);
 			switch(choice){
 				case '1' : System.out.print("Enter two number: ");
 						   a = scan.nextFloat();
@@ -16,11 +18,28 @@ public class Calculator{
 						   res = a + b;
 						   System.out.print("Result = " + res);
 						   break;
-				case '2' : System.exit(0);
+				case '2' : System.out.print("Enter two number: ");
+						   a = scan.nextFloat();
+						   b = scan.nextFloat();
+						   res = a * b;
+						   System.out.print("Result = " + res);
+						   break;
+				case '3' : System.out.print("Enter two number: ");
+						   a = scan.nextFloat();
+						   b = scan.nextFloat();
+						   res = a - b;
+						   System.out.print("Result = " + res);
+						   break;
+				case '4' : System.out.print("Enter two number: ");
+						   a = scan.nextFloat();
+						   b = scan.nextFloat();
+						   res = a / b;
+						   System.out.print("Result = " + res);
+						   break;
+				case '5' : System.exit(0);
 							break;
 				default : System.out.print("INVALID CHOICE!!!");
 						  break;
 			}
-		}
 	}
 }
